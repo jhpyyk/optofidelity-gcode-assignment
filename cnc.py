22d953176c90423e4b1703a2af3d5f619ff4fbcb
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2023-01-19 20:26:36 +0200

    Refactor parse_lines to parse_file and add a comment to it

--- cnc.py
+++ cnc.py
@@ -7,15 +7,20 @@ def main(args):
         print("Invalid argument: not a gcode file")
         return
 
-    lines = parse_lines(file_path)
-    print(lines)
+    lines = parse_file(file_path)
+    
+    
 
-def parse_lines(file_path):
+def parse_file(file_path):
     """ 
     Parses lines from a G-Code file.
 
     Args:
-    gcode_file: 
+    file_path (string): Path to the G-code file.
+
+    Returns:
+    lines (array): Array of all the lines. Each line is split into strings with
+                   space being the separator.
     """
 
     gcode_file = open(file_path)
