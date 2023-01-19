1a0ca097757cdad906e1b2fb9d0383adc02f1cb1
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2023-01-19 22:03:04 +0200

    Refactor cnc.py into class Parser.py

--- cnc.py
+++ cnc.py
@@ -1,76 +1,12 @@
-import MachineClient
 import sys
 
+import MachineClient
+import Parser
+
 def main(args):
-    file_path = "./{}".format(args[1])
-    if (file_path[-6:] != '.gcode'):
-        print("Invalid argument: not a gcode file")
-        return
-
-    lines = parse_file_to_lines(file_path)
-    commands = parse_lines_to_commands(lines)
+    parser = Parser.Parser()
+    commands = parser.parse_file(args[1])
     print(commands)
-    
-    
-
-def parse_file_to_lines(file_path):
-    """ 
-    Parses lines from a G-Code file.
-
-    Args:
-    file_path (string): Path to the G-code file.
-
-    Returns:
-    lines (array): Array of all the lines. Each line is split into strings with
-                   space being the separator.
-    """
-
-    gcode_file = open(file_path)
-    line_arr = []
-
-    while True:
-        line = gcode_file.readline()
-
-        if not line:
-            break
-        
-        if (line[0] == 'N'):
-            line = line.strip()
-            line = line.strip('\n')
-            line_arr.append(line.split(" "))
-
-    gcode_file.close()
-    
-    return line_arr
-
-def parse_lines_to_commands(lines):
-    """
-    Parses commands from an array of parsed G-Code lines.
-
-    Args:
-    lines (array): Array of parsed G-Code lines.
-
-    Returns:
-    commands (array): Array of commands.
-    """
-
-    command_arr = []
-    command_buffer = []
-    coordinate_args = {'X', 'Y', 'Z'}
-
-    for line in lines:
-        line_number = line[0]
-        line = line[1:]
-        for code in line:
-            if (code[0] in coordinate_args):
-                command_buffer.append(code)
-            else:
-                command_arr.append(command_buffer)
-                command_buffer = []
-                command_buffer.append(code)
-
-    return command_arr
-
 
 if __name__ == "__main__":
     main(sys.argv)
