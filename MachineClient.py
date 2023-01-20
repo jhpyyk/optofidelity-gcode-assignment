6395a00eb713ae9c3d7238f7b2c6e001758ff579
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2023-01-20 15:07:32 +0200

    Add missing stubs to MachineClient.py

--- MachineClient.py
+++ MachineClient.py
@@ -6,7 +6,6 @@ class MachineClient:
     def home(self):
         """ Moves machine to home position. """
         print("Moving to home.")
-        time.sleep(2)
         
     def move(self, x, y, z):
         """ Uses linear movement to move spindle to given XYZ coordinates.
@@ -17,7 +16,6 @@ class MachineClient:
         z (float): Z axis absolute value [mm]
         """
         print("Moving to X={:.3f} Y={:.3f} Z={:.3f} [mm].".format(x, y, z))
-        time.sleep(2)
 
 
     def move_x(self, value):
@@ -27,7 +25,6 @@ class MachineClient:
         value (float): Axis absolute value [mm]
         """
         print("Moving X to {:.3f} [mm].".format(value))
-        time.sleep(2)
 
     def move_y(self, value):
         """ Move spindle to given Y coordinate. Keeps current X and Z unchanged.
@@ -36,7 +33,6 @@ class MachineClient:
         value(float): Axis absolute value [mm]
         """
         print("Moving Y to {:.3f} [mm].".format(value))
-        time.sleep(2)
 
     def move_z(self, value):
         """ Move spindle to given Z coordinate. Keeps current X and Y unchanged.
@@ -45,41 +41,93 @@ class MachineClient:
         value (float): Axis absolute value [mm]
         """
         print("Moving Z to {:.3f} [mm].".format(value))
-        time.sleep(2)
 
     def set_feed_rate(self, value):
         """ Set spindle feed rate.
+        G-code: F.
 
         Args:
         value (float): Feed rate [mm/s]
         """
         print("Using feed rate {} [mm/s].".format(value))
-        time.sleep(2)
 
     def set_spindle_speed(self, value):
         """ Set spindle rotational speed.
+        G-code: S.
 
         Args:
         value (int): Spindle speed [rpm]
         """
         print("Using spindle speed {} [mm/s].".format(value))
-        time.sleep(2)
 
     def change_tool(self, tool_name):
         """ Change tool with given name.
+        G-code : M06.
 
         Args:
         tool_name (str): Tool name.
         """
         print("Changing tool '{:s}'.".format(tool_name))
-        time.sleep(2)
 
     def coolant_on(self):
         """ Turns spindle coolant on. """
         print("Coolant turned on.")
-        time.sleep(2)
 
     def coolant_off(self):
         """ Turns spindle coolant off. """
         print("Coolant turned off.")
-        time.sleep(2)
+
+    def rapid_positioning(self, x, y, z):
+        """ Move spindle to given XYZ coordinates at max speed.
+        G-code: G00.
+
+        Args:
+        x (float): X axis absolute value [mm]
+        y (float): Y axis absolute value [mm]
+        z (float): Z axis absolute value [mm]
+        """
+        print("Moving to X={:.3f} Y={:.3f} Z={:.3f} [mm].".format(x, y, z))
+
+    def plane_XY(self):
+        """ Set interpolation plane to XY. G-code: G17 """
+        print("Set interpolation plane to XY.")
+
+    def set_millimeters(self):
+        """ Set programming to use millimeters. G-code: G21 """
+        print("Set programming to use millimeters.")
+
+    def turn_off_cutter_radius_c(self):
+        """ Turn off cutter radius compensation. G-code: G40 """
+        print("Turn off cutter radius compensation.")
+
+    def tool_length_c_cancel(self):
+        """ Cancel tool length offset compensation. G-code: G49 """
+        print("Cancel tool length offset compensation.")
+
+    def canned_cycle_cancel(self):
+        """ Cancel canned cycle. G-code: G80 """
+        print("Cancel canned cycle.")
+
+    def set_feed_rate_pm(self):
+        """ Set feed rate per minute. G-code: G94 """
+        print("Set feed rate per minute.")
+
+    def tool_select(self, tool_name):
+        """ Select tool. G-code = T."""
+        print("Select tool {}".format(tool_name))
+
+    def set_spindle_on(self):
+        """ Set spindle on. G-code: M03. """
+        print( "Set spindle on.")
+
+    def set_absolute_programming(self):
+        """ Set absolute programming. G-code: G90. """
+        print("Set absolute programming.")
+
+    def set_work_coordinate_system(self):
+        """ Set work coordinate system. G-code: G54. """
+        print("Set work coordinate system")
+
+    def set_incremental_programming(self):
+        """ Set incremental programming. G-code: G91. """
+        print("Set incremental programming.")
--- cnc.py
+++ cnc.py
@@ -1,12 +1,37 @@
 import sys
 
-import MachineClient
+import MachineClient as MC
 import Parser
+
+COMMAND_TO_MC_STUB = {'G00' : move_logic,
+                      'G17' : MC.plane_XY,
+                      'G21' : MC.set_millimeters,
+                      'G40' : MC.turn_off_cutter_radius_c,
+                      'G49' : MC.tool_length_c_cancel,
+                      'G80' : MC.canned_cycle_cancel,
+                      'G94' : MC.set_feed_rate_pm,
+                      'T'   : MC.tool_select,
+                      'M06' : MC.change_tool,
+                      'S'   : MC.set_spindle_speed,
+                      'M03' : MC.set_spindle_on,
+                      'G90' : MC.set_absolute_programming,
+                      'G54' : MC.set_work_coordinate_system,
+                      'G01' : move_logic,
+                      'F'   : MC.set_feed_rate,
+                      'G91' : MC.set_incremental_programming,
+                      'G28' : MC.home
+                      }
 
 def main(args):
     parser = Parser.Parser()
     commands = parser.parse_file(args[1])
+
+
     print(commands)
+
+def move_logic():
+    return
+
 
 if __name__ == "__main__":
     main(sys.argv)
